32ea0c6cff7a15608191519f29a78a71d932a033
Mateusz Siller <mateusz.siller@gmail.com>
2024-03-02 10:24:27 +0100

    animate-number

--- src/main.ts
+++ src/main.ts
@@ -19,3 +19,41 @@ navClose.addEventListener('click', () => {
 		{ once: true }
 	)
 })
+
+function easeOutSine(x: number): number {
+	return Math.sin((x * Math.PI) / 2)
+}
+
+function animateNumbers(element: HTMLDivElement, duration: number) {
+	const target = parseInt(element.getAttribute('target')!)
+	const start = performance.now()
+
+	function updateCount() {
+		let t = (performance.now() - start) / duration
+		if (t < 1) {
+			t = Math.min(1, t)
+			element.textContent = Math.floor(target * easeOutSine(t)).toString()
+			requestAnimationFrame(updateCount)
+		} else {
+			element.textContent = Math.floor(target).toString()
+		}
+	}
+
+	updateCount()
+}
+
+const observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				animateNumbers(entry.target as HTMLDivElement, 1000)
+				observer.unobserve(entry.target)
+			}
+		})
+	},
+	{ threshold: 1 }
+)
+
+document.querySelectorAll('[animate-number]').forEach((el) => {
+	observer.observe(el)
+})
